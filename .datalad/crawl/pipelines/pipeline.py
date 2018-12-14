afe80e089f494277571e96fb70815278d0c1403d
Michael Hanke <michael.hanke@gmail.com>
2018-12-14 19:42:56 +0100

    Initial pipeline draft

---
+++ .datalad/crawl/pipelines/pipeline.py
@@ -0,0 +1,37 @@
+import requests
+from datalad_crawler.consts import ARCHIVES_SPECIAL_REMOTE
+from datalad_crawler.nodes.annex import Annexificator
+from datalad_crawler.pipelines.simple_with_archives \
+    import pipeline as sa_pipeline
+
+
+def get_studytarball_urls(_):
+    r = requests.get('http://anima.fz-juelich.de/api/archives')
+    for url in r.json():
+        yield dict(
+            url=url,
+        )
+
+
+def pipeline():
+    """Pipeline to crawl study datasets through DB queries
+    """
+
+    annex = Annexificator(
+        create=False,  # must be already initialized etc
+        backend='MD5E',
+        statusdb='json',
+        special_remotes=[ARCHIVES_SPECIAL_REMOTE],
+        # every single file should go into the annex
+        largefiles="anything",
+    )
+
+    incoming_pipeline = [
+        get_studytarball_urls,
+        annex,
+    ]
+
+    return sa_pipeline(
+        rename=[':(.*)\.study:.metadata/\\1.study'],
+        incoming_pipeline=incoming_pipeline,
+    )
